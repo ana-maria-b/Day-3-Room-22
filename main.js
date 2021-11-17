6466a9d553361c3cb5429a7d43b2ab75281ea02b
“Ana-Maria” <mariaana1491@gmail.com>
2021-11-17 14:51:17 +0000

    JS101 Baby

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("HELLO WORLD");
+let myFavNumber = 80;
+if (myFavNumber === 5) {
+  console.log("My favourite number is 5 too!");
+}
+
+let answer = prompt("What's your name?");
+if (answer === "Ben") {
+  console.log("That's my name too!");
+} else {
+  console.log(answer + "That's a silly name...");
+}
+
+let weatherType = "sunny";
+if (weatherType === "raining") {
+  console.log("Take an umbrella!");
+} else if (weatherType === "sunny") {
+  console.log("Don't forget suncream!");
+} else {
+  console.log("Must be a normal grey day");
+}
